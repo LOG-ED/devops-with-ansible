8abc8b39e1c584759d6be7090eefac6ced00a736
Jacopo Secchiero <secchierojacopo@gmail.com>
2017-04-11 18:58:05 +0200

    completamento corso - lezione 3

---
+++ 3/os-openrc.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export OS_REGION_NAME="nl-ams1"
+export OS_AUTH_URL="https://api.${OS_REGION_NAME}.entercloudsuite.com/v2.0"
+export OS_TENANT_NAME=""
+export OS_USERNAME="${OS_TENANT_NAME}"
+export OS_PASSWORD=""
